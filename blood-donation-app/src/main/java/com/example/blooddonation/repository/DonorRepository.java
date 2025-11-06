de907e66e2fde08d78e0ea778bd00d922c550636
rishitasharmin6-max <rishitasharmin6@gmail.com>
2025-11-06 08:00:50 +0600

    Delete blood-donation-app folder from repo

--- blood-donation-app/src/main/java/com/example/blooddonation/repository/DonorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.blooddonation.repository;
-
-import com.example.blooddonation.model.Donor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DonorRepository extends JpaRepository<Donor, Long> { }
--- blood-donation-app/src/main/java/com/example/blooddonation/repository/RequestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.blooddonation.repository;
-
-import com.example.blooddonation.model.Request;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RequestRepository extends JpaRepository<Request, Long> { }
